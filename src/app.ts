666a123df04eed6b16c4daeb6a864d488c142ed4
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-08-01 09:55:40 -0300

    feat: Configura a rota para criação de no usuario

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import 'express-async-errors';
 import erroMiddleware from './middlewares/erro.middleware';
 import productsRoute from './routes/Products.routes';
+import usersRoute from './routes/User.routes';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/products', productsRoute);
+app.use('/users', usersRoute);
 
 app.use(erroMiddleware);
 
