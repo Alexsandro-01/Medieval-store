9ed8baaed2552fcadd8b41fe29a03414bc388676
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-08-02 10:17:42 -0300

    feat: adiciona rota para criação de nova ordem de compra

--- src/routes/Orders.routes.ts
+++ src/routes/Orders.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import tokenMiddleware from '../middlewares/token.middleware';
 import ordersController from '../controllers/orders.controller';
 
 const route = Router();
 
 route.get('/', ordersController.getAll);
+route.post('/', tokenMiddleware, ordersController.create);
 
 export default route;
